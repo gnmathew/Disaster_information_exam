a09472016ad92f3800266763255fd4f0ca8b43e1
gnmathew <genemathewdorado@gmail.com>
2023-07-07 16:44:17 +0800

    added user authentication for post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,10 @@ class Post < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
   validates :address, presence: true
+
+  belongs_to :user
+
+  def destroy
+    update(deleted_at: Time.current)
+  end
 end
