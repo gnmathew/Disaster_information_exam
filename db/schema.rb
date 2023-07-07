c09da7f3bced61584dce71b442ab7e1bd58ab1fc
gnmathew <genemathewdorado@gmail.com>
2023-07-07 21:22:40 +0800

    added comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_07_090121) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_07_124229) do
+  create_table "comments", charset: "utf8mb4", force: :cascade do |t|
+    t.string "content"
+    t.bigint "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
+
   create_table "posts", charset: "utf8mb4", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -34,4 +42,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_07_090121) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "posts"
 end
